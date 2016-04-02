54ee85ca608ee357b021c5b76c4ee812d533ae64
pkoltun <pkdev14@gmail.com>
2016-04-02 21:14:29 +0200

    Update the app to have explicit list of clients

--- app.js
+++ app.js
@@ -9,14 +9,12 @@ var SocketioServer = require("sensors-io-web").Server;
 var gpioSensors = rpiSensors.GPIOSensors;
 
 // -------------------Configuration------------------------------------------------
+var clients= [/*Replace this with IPs of device which should be able to recive audio/video streams*/]; // e.g. clients= ["192.168.1.2","192.168.1.3"];
+
 // Default port used by node app.
 var port = process.env.port || 1337;
 
 var multicastInterface ="eth0"; 
-
-// Linux audio input and output device.
-var inputDevice = "plughw:Set";
-var outputDevice = "plughw:Set";
 
 // The sound played outside when button is pressed. Empty string means that default sound is used.
 var soundFile = "";
@@ -27,12 +25,26 @@ var buttonPin = 27;
 //Door lock pin
 var doorLockPin = 17;
 
+var audioCodec ="raw";
+
 //--END-------------------
 
 var serverPair = SocketioServer.configureSimpleServers(http, port, true);
 var sensorsServer = serverPair.sensorsServer;
 var server = serverPair.httpServer;
 
+if (clients.length === 0){
+		console.log("Please edit app.js and provide IPs of clients.")
+	return;
+}
+
+var getClientsString = function( port ){
+    var result = clients[0]+":"+port;
+    for ( i = 1; i<clients.length; i++){
+        result += "," + clients[i]+":"+port;
+    }
+    return result;
+}
 
 var outGPIO = new gpioSensors.OutGPIOSensor(false, doorLockPin);
 
@@ -52,15 +64,15 @@ setInterval(changeState, 3000);
 
 var button = new gpioSensors.InGPIOSensor(buttonPin);
 
-var camera = new rpiGS.Camera();
+var camera = new rpiGS.Camera(null, true, null);
 var speaker = new rpiGS.Speaker(null, true, null);
-var mic = new rpiGS.Microphone();
+var mic = new rpiGS.Microphone(null, true, null);
 var ring = new rpiRing.Ring();
 
-camera.setState( { on: true,multicastIface: multicastInterface });  
-mic.setState({ device: inputDevice, multicastIface: multicastInterface});
-speaker.setState({ device: outputDevice,multicastIface: multicastInterface});
-ring.setState({ device: outputDevice,multicastIface: multicastInterface});
+camera.setState( { on: true,multicastIface: multicastInterface, clients: getClientsString(5000) });
+mic.setState({ on:true, multicastIface: multicastInterface, clients: getClientsString(5001), audioCodec: audioCodec});
+speaker.setState({ on:true, multicastIface: multicastInterface, clients: getClientsString(5002), audioCodec: audioCodec});
+ring.setState({ multicastIface: multicastInterface});
 
 if ( soundFile !== ""){
 	ring.setState( {soundFile: soundFile});
